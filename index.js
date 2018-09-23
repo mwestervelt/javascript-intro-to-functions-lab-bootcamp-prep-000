b754a0200dccb6aeecfbf6983f95adb03fa201d5
Mariel Westervelt <elle.westervelt@gmail.com>
2018-09-23 18:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string) {
   console.log ("hello")
 }
 function sayHiToGrandma(string) {
-  if logWhisper(string) === string.toUpperCase()
+  if string === string.toUpperCase()
     return ("YES INDEED!"),
   else if string === string.toLowerCase()
     return ("I can't hear you!"),
-  else if string === ("I love you, Grandma")
+  else if string === ("I love you, Grandma.")
     return ("I love you, too.")
 }
